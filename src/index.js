69c57576ca0d7d9472b9280b9263a40406a23c9a
Svetlana Codrean <codrean.sveta@gmail.com>
2025-12-29 15:35:49 +0100

    Create homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css"
-import leavesImage from "./leaves.png"
+// import leavesImage from "./leaves.png"
 
-const image = document.createElement("img")
-image.src = leavesImage
-document.body.appendChild(image)
+// const image = document.createElement("img")
+// image.src = leavesImage
+// document.body.appendChild(image)

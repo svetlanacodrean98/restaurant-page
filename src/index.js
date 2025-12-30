e99f32ed5ef44b54a6b8e3a30c8517f214ac6bc4
Svetlana Codrean <codrean.sveta@gmail.com>
2025-12-30 19:16:25 +0100

    Add home, menu and about tabs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./styles.css"
-import {initialLoad} from "./module.js"
+import {initialLoad, openPage} from "./module.js"
 
 window.onload = initialLoad
+
+openPage("homebtn", "home")
+openPage("aboutbtn", "about")
+openPage("menubtn", "menu")
--- src/module.js
+++ src/module.js
@@ -9,3 +9,20 @@ export function initialLoad() {
     description.innerHTML = "Svetlana has a variety of foods prepared for you! From russian cuisine to original recipes, cooked with love!"
     content.appendChild(description)
 }
+
+export function openPage(btnId, tabId) {
+
+    const buttonHome = document.getElementById(btnId)
+    buttonHome.addEventListener("click", (event) => {
+    let tabcontents = document.getElementsByClassName("tabcontent")
+    for (let i=0; i<tabcontents.length; i++) {
+        tabcontents[i].style.display = "none"
+    }
+    let tablinks = document.getElementsByClassName("tablink")
+    for (let i=0; i<tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "")
+    }
+    document.getElementById(tabId).style.display = "block"
+    event.currentTarget.className += "active"
+})
+}

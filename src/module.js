418be87f4a6b00704b21ec0124d3ded37fdbd2af
Svetlana Codrean <codrean.sveta@gmail.com>
2025-12-31 20:49:23 +0100

    Fix active button functionality

--- src/module.js
+++ src/module.js
@@ -11,18 +11,17 @@ export function initialLoad() {
 }
 
 export function openPage(btnId, tabId) {
-
-    const buttonHome = document.getElementById(btnId)
-    buttonHome.addEventListener("click", (event) => {
-    let tabcontents = document.getElementsByClassName("tabcontent")
-    for (let i=0; i<tabcontents.length; i++) {
-        tabcontents[i].style.display = "none"
-    }
-    let tablinks = document.getElementsByClassName("tablink")
-    for (let i=0; i<tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace("active", "")
-    }
-    document.getElementById(tabId).style.display = "block"
-    event.currentTarget.className += "active"
-})
+    const button = document.getElementById(btnId)
+    button.addEventListener("click", (event) => {
+        let tabcontents = document.getElementsByClassName("tabcontent")
+        for (let i=0; i<tabcontents.length; i++) {
+            tabcontents[i].style.display = "none"
+        }
+        let tablinks = document.getElementsByClassName("tablink")
+        for (let i=0; i<tablinks.length; i++) {
+            tablinks[i].classList.remove("active")
+        }
+        document.getElementById(tabId).style.display = "block"
+        event.currentTarget.classList.add("active")
+    })
 }
